a5218764f9c36d6d7ccb33d647fcaf7910fd7468
Lohannecristina <20220085@iade.pt>
2024-11-08 21:15:31 +0000

    Adição da tela de histórico

---
+++ app/app/src/main/java/pt/iade/ei/thinktoilet/ui/components/HistoryCard.kt
@@ -0,0 +1,95 @@
+package pt.iade.ei.thinktoilet.ui.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import pt.iade.ei.thinktoilet.models.Toilet
+import pt.iade.ei.thinktoilet.test.generateRandomToilet
+import pt.iade.ei.thinktoilet.ui.theme.AppTheme
+
+@Composable
+fun HistoryCard(
+    toilet: Toilet
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 20.dp,
+                vertical = 5.dp
+            )
+
+            .border(
+                width = 2.dp,
+                color = Color.LightGray,
+                shape = MaterialTheme.shapes.medium
+            ),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest.copy(alpha = 0.5f),
+            disabledContentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+        ),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 25.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            Column(
+                modifier = Modifier.weight(1f)
+            )
+            {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = toilet.name,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+            Column(modifier = Modifier.padding(horizontal = 10.dp),
+            )
+            {
+                Stars(
+                    rating = toilet.getAverageRating(),
+                )
+
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileCard() {
+    AppTheme {
+        HistoryCard(
+            toilet = generateRandomToilet()
+        )
+    }
+}
--- app/app/src/main/java/pt/iade/ei/thinktoilet/ui/screens/HistoryScreen.kt
+++ app/app/src/main/java/pt/iade/ei/thinktoilet/ui/screens/HistoryScreen.kt
@@ -3,30 +3,37 @@ package pt.iade.ei.thinktoilet.ui.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import pt.iade.ei.thinktoilet.models.Toilet
+import pt.iade.ei.thinktoilet.test.generateRandomToilet
+import pt.iade.ei.thinktoilet.test.generateRandomToilets
+import pt.iade.ei.thinktoilet.ui.components.HistoryCard
 
 @Composable
-fun HistoryScreen(
-    navController: NavController = rememberNavController(),
-) {
+fun HistoryScreen() {
+    val toilets: List<Toilet> = generateRandomToilets(20)
+
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    )
-    {
-        Column {
-            Text(
-                text = "History",
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                fontSize = 100.sp
-            )
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 20.dp),
+    ) {
+        LazyColumn {
+            items(toilets) { toilet ->
+                HistoryCard(
+                    toilet = toilet,
+
+                )
+            }
         }
     }
 }
