900ad1809e713571db51a9b1d7a8c80528977df9
nycocado <nycolascanutto@gmail.com>
2025-11-24 20:57:37 +0000

    Refatoração na documentação do health

--- apps/api/src/modules/health/health.controller.ts
+++ apps/api/src/modules/health/health.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheck,
+  HealthCheckResult,
   HealthCheckService,
   MemoryHealthIndicator,
 } from '@nestjs/terminus';
 import { ApiSwaggerHealthCheck } from './swagger';
 
+/**
+ * Gerencia as requisições HTTP para operações relacionadas ao health check.
+ */
 @Controller('health')
 export class HealthController {
   constructor(
@@ -13,10 +17,14 @@ export class HealthController {
     private memory: MemoryHealthIndicator,
   ) {}
 
+  /**
+   * Verifica o estado de saúde da aplicação.
+   * @returns {Promise<HealthCheckResult>} O resultado do health check.
+   */
   @Get()
   @HealthCheck()
   @ApiSwaggerHealthCheck()
-  check() {
+  check(): Promise<HealthCheckResult> {
     return this.health.check([
       () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024), // 150MB
     ]);
