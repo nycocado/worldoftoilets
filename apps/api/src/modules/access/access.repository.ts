2dbfe923457acc6373025f83f292535565a6fa03
nycocado <nycolascanutto@gmail.com>
2025-11-24 20:55:03 +0000

    Refatoração na documentação do access

--- apps/api/src/modules/access/access.repository.ts
+++ apps/api/src/modules/access/access.repository.ts
@@ -3,6 +3,9 @@ import { InjectRepository } from '@mikro-orm/nestjs';
 import { AccessApiName, AccessEntity } from '@database/entities';
 import { EntityRepository } from '@mikro-orm/mariadb';
 
+/**
+ * Gerencia o acesso e a persistência de dados para a entidade Access.
+ */
 @Injectable()
 export class AccessRepository {
   constructor(
@@ -10,6 +13,12 @@ export class AccessRepository {
     private readonly repository: EntityRepository<AccessEntity>,
   ) {}
 
+  /**
+   * Busca um acesso pelo seu nome de API.
+   *
+   * @param {AccessApiName} apiName O nome da API do acesso a ser buscado.
+   * @returns {Promise<AccessEntity | null>} A entidade de acesso ou `null` se não for encontrada.
+   */
   async findByApiName(apiName: AccessApiName): Promise<AccessEntity | null> {
     return this.repository.findOne({ apiName });
   }
--- apps/api/src/modules/access/constants/exceptions.constant.ts
+++ apps/api/src/modules/access/constants/exceptions.constant.ts
@@ -1,3 +1,6 @@
+/**
+ * Contém as mensagens de exceção para o módulo de acesso.
+ */
 export const ACCESS_EXCEPTIONS = {
   ACCESS_NOT_FOUND: 'Access type not found.',
 };
