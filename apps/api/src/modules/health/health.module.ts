8115f0d9d10d18c37385a1c1c0b4f256f4f05875
nycocado <nycolascanutto@gmail.com>
2025-11-24 20:57:37 +0000

    [docs] rework health documentation

--- apps/api/src/modules/health/health.module.ts
+++ apps/api/src/modules/health/health.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from '@modules/health/health.controller';
 
+/**
+ * Gerencia a funcionalidade de health check, agrupando seus componentes.
+ */
 @Module({
   imports: [TerminusModule],
   controllers: [HealthController],
