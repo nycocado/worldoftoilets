f5008515e9c6a327a6a4823f3a37df33e4ec7409
nycocado <nycolascanutto@gmail.com>
2025-11-25 15:49:38 +0000

    Validação de reply apenas nos comentarios que o partner é associado

--- apps/api/src/modules/reply/constants/exceptions.constant.ts
+++ apps/api/src/modules/reply/constants/exceptions.constant.ts
@@ -5,4 +5,6 @@ export const REPLY_EXCEPTIONS = {
   REPLY_NOT_FOUND: 'Reply not found',
   REPLY_NOT_OWNED: 'You do not own this reply',
   REPLY_DELETED: 'Reply has been deleted',
+  PARTNER_CAN_ONLY_REPLY_TO_OWN_TOILET:
+    'Partners can only reply to comments on their associated toilet',
 };
