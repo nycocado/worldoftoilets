51ec8a003aa8dfdbb213c3e2c5a5916f4a400c4e
nycocado <nycolascanutto@gmail.com>
2025-11-25 13:07:42 +0000

    [docs] rework role documentation

--- apps/api/src/modules/role/role.module.ts
+++ apps/api/src/modules/role/role.module.ts
@@ -4,6 +4,9 @@ import { RoleService } from './role.service';
 import { RoleRepository } from '@modules/role/role.repository';
 import { RoleEntity } from '@database/entities';
 
+/**
+ * Gerencia a funcionalidade de cargos, agrupando seus componentes.
+ */
 @Module({
   imports: [MikroOrmModule.forFeature([RoleEntity])],
   providers: [RoleService, RoleRepository],
