34a15dbc4e5385ee86e99a2277e075ef464596ae
nycocado <nycolascanutto@gmail.com>
2025-11-25 12:49:34 +0000

    Refatoração na documentação do refresh-token

--- apps/api/src/modules/refresh-token/constants/exceptions.constant.ts
+++ apps/api/src/modules/refresh-token/constants/exceptions.constant.ts
@@ -1,3 +1,6 @@
+/**
+ * Contém as mensagens de exceção para o módulo de refresh token.
+ */
 export const REFRESH_EXCEPTIONS = {
   REFRESH_TOKEN_INVALID: 'Invalid refresh token.',
   REFRESH_TOKEN_EXPIRED: 'Refresh token expired.',
