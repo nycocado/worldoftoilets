1924fe7a121fd316f3f868c010ee3299ec9142da
nycocado <nycolascanutto@gmail.com>
2025-11-25 13:21:35 +0000

    Refatoração na documentação do user-credential

--- apps/api/src/modules/user-credential/user-credential.module.ts
+++ apps/api/src/modules/user-credential/user-credential.module.ts
@@ -4,6 +4,9 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UserCredentialEntity } from '@database/entities';
 import { UserCredentialRepository } from '@modules/user-credential/user-credential.repository';
 
+/**
+ * Gerencia a funcionalidade de credenciais de utilizador, agrupando seus componentes.
+ */
 @Module({
   imports: [MikroOrmModule.forFeature([UserCredentialEntity])],
   providers: [UserCredentialService, UserCredentialRepository],
