fefd298a7f1b8a338e0313cc9785d9400995b93f
nycocado <nycolascanutto@gmail.com>
2025-11-25 12:43:43 +0000

    [docs] rework minio documentation

--- apps/api/src/modules/minio/minio.module.ts
+++ apps/api/src/modules/minio/minio.module.ts
@@ -2,6 +2,9 @@ import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MinioService } from './minio.service';
 
+/**
+ * Gerencia a funcionalidade de Minio (armazenamento de objetos), agrupando seus componentes.
+ */
 @Global()
 @Module({
   imports: [ConfigModule],
